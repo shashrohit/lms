d3d5d7f210ff814a2548047768b4f56bc6901709
Singh <shashank.singh9@stryker.com>
2020-12-26 16:01:05 +0530

    unit tests

---
+++ library/tests/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from library.models import Book
+import json
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('list books')
+        self.detail_url = reverse('book details')
+
+    def test_list_available_books(self):
+
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('list_books.html')
+
+    def test_book_details(self):
+        response = self.client.get(self.detail_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('book_details.html')
+
+
