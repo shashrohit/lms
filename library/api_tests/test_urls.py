32dd29d7dd782f5240c4caaf616ff20bd80e9356
Singh <shashank.singh9@stryker.com>
2021-01-03 00:06:05 +0530

    api tests

--- library/api_tests/test_urls.py
+++ library/api_tests/test_urls.py
@@ -6,4 +6,4 @@ def test_list_books():
     service_url = environ.get("service_url")
     list_url = service_url + "/list-books"
     response = requests.get(list_url)
-    assert response.status_code == 201
+    assert response.status_code == 200
