8304ede0379ea979179564de61bd7b156480ff63
Singh <shashank.singh9@stryker.com>
2021-01-02 22:54:17 +0530

    unit test stage

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -17,7 +17,5 @@ class TestViews(TestCase):
 
     def test_book_details(self):
         response = self.client.get(self.detail_url)
-        self.assertEquals(response.status_code, 201)
+        self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed('book_details.html')
-
-
